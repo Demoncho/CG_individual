fdca4f3589f6ca4c55b4c55e4ba93ff0b345be64
solntsev.d <32107379+Demoncho@users.noreply.github.com>
2019-10-09 22:52:11 +0300

    Add files via upload

---
+++ CG_individ/CG_individ/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace CG_individ
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.canvas = new System.Windows.Forms.PictureBox();
+            this.button_clear = new System.Windows.Forms.Button();
+            this.button_algoritm = new System.Windows.Forms.Button();
+            this.label_help = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // canvas
+            // 
+            this.canvas.Location = new System.Drawing.Point(169, 1);
+            this.canvas.Name = "canvas";
+            this.canvas.Size = new System.Drawing.Size(636, 449);
+            this.canvas.TabIndex = 0;
+            this.canvas.TabStop = false;
+            this.canvas.MouseDown += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseDown);
+            this.canvas.MouseMove += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseMove);
+            this.canvas.MouseUp += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseUp);
+            // 
+            // button_clear
+            // 
+            this.button_clear.Location = new System.Drawing.Point(30, 94);
+            this.button_clear.Name = "button_clear";
+            this.button_clear.Size = new System.Drawing.Size(107, 23);
+            this.button_clear.TabIndex = 1;
+            this.button_clear.Text = "Очистить поле";
+            this.button_clear.UseVisualStyleBackColor = true;
+            this.button_clear.Click += new System.EventHandler(this.button_clear_Click);
+            // 
+            // button_algoritm
+            // 
+            this.button_algoritm.Location = new System.Drawing.Point(30, 144);
+            this.button_algoritm.Name = "button_algoritm";
+            this.button_algoritm.Size = new System.Drawing.Size(107, 23);
+            this.button_algoritm.TabIndex = 2;
+            this.button_algoritm.Text = "Применить";
+            this.button_algoritm.UseVisualStyleBackColor = true;
+            this.button_algoritm.Click += new System.EventHandler(this.button_algoritm_Click);
+            // 
+            // label_help
+            // 
+            this.label_help.AutoSize = true;
+            this.label_help.Location = new System.Drawing.Point(27, 211);
+            this.label_help.Name = "label_help";
+            this.label_help.Size = new System.Drawing.Size(109, 13);
+            this.label_help.TabIndex = 3;
+            this.label_help.Text = "Недостаточно точек";
+            this.label_help.Visible = false;
+            this.label_help.Click += new System.EventHandler(this.label_help_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label_help);
+            this.Controls.Add(this.button_algoritm);
+            this.Controls.Add(this.button_clear);
+            this.Controls.Add(this.canvas);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox canvas;
+        private System.Windows.Forms.Button button_clear;
+        private System.Windows.Forms.Button button_algoritm;
+        private System.Windows.Forms.Label label_help;
+    }
+}
+
---
+++ CG_individ/CG_individ/Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CG_individ
+{
+    public partial class Form1 : Form
+    {
+        bool drawing = true;
+        Bitmap bmp;
+        Graphics g;
+        Point from, to;
+        Pen pen = new Pen(Color.Black);
+        List<Point> polygon_points = new List<Point>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            canvas.Image = new Bitmap(canvas.Width, canvas.Height);           
+            g = Graphics.FromImage(canvas.Image);
+            g.Clear(Color.White);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_clear_Click(object sender, EventArgs e)
+        {
+            polygon_points.Clear();
+            drawing = true;
+            label_help.Visible = false;
+            canvas.Image = new Bitmap(canvas.Width, canvas.Height);
+            g = Graphics.FromImage(canvas.Image);
+            g.Clear(Color.White);
+            canvas.Refresh();
+        }
+
+        private void button_algoritm_Click(object sender, EventArgs e)
+        {
+            if (polygon_points.Count <= 2)
+            { 
+                label_help.Visible = true;
+            }
+            else
+            {
+                label_help.Visible = false;               
+                drawing = false;
+                Point left = polygon_points[0];
+                Point right = polygon_points[0];
+                for (int i = 0; i < polygon_points.Count; ++i)
+                {
+                    if (polygon_points[i].X < left.X)
+                        left = polygon_points[i];
+                    if (polygon_points[i].X > right.X)
+                        right = polygon_points[i];
+                }
+                List<Point> new_polygon_points = polygon_points;
+                new_polygon_points.Remove(left);
+                new_polygon_points.Remove(right);
+                List<Point> above = above_points(left, right, new_polygon_points);
+                List<Point> below = below_points(left, right, new_polygon_points);
+                up_border(left, right, above);
+                down_border(left, right, below);
+            }
+        }
+
+        private void canvas_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (drawing)
+            { 
+                from = e.Location;
+                to = e.Location;
+            }
+        }
+
+        private void canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (drawing)
+            {
+                to = e.Location;
+                canvas.Invalidate();
+            }
+        }
+
+        private void canvas_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (drawing)
+            {
+                g.FillEllipse(new SolidBrush(Color.Black), new Rectangle(to.X, to.Y, 3, 3));
+                polygon_points.Add(new Point(to.X, to.Y));
+            }
+        }
+
+        private void label_help_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private List<Point> above_points(Point left, Point right, List<Point> src)
+        {
+            List<Point> result = new List<Point>();
+            for (int i = 0; i<src.Count; ++i)
+            {
+                if (what_side(left, right, src[i]) > 0)
+                    result.Add(src[i]);
+            }
+            return result;
+        }
+
+        private List<Point> below_points(Point left, Point right, List<Point> src)
+        {
+            List<Point> result = new List<Point>();
+            for (int i = 0; i < src.Count; ++i)
+            {
+                if (what_side(left, right, src[i]) < 0)
+                    result.Add(src[i]);
+            }
+            return result;
+        }
+
+        private void up_border(Point left, Point right, List<Point> arr)
+        {
+            if (arr.Count == 0)
+            {
+                //canvas.Image = new Bitmap(canvas.Width, canvas.Height);
+                //g = Graphics.FromImage(canvas.Image);
+                g.DrawLine(pen, left, right);
+                canvas.Invalidate();
+                //canvas.Refresh();
+            }
+            else
+            {
+                Point far = far_point(left, right, arr);
+                List<Point> new_arr = arr;
+                new_arr.Remove(far);
+                List<Point> above_left = above_points(left, far, new_arr);
+                List<Point> above_right = above_points(far, right, new_arr);
+                up_border(left, far, above_left);
+                up_border(far, right, above_right);
+            }
+        }
+        
+        private void down_border(Point left, Point right, List<Point> arr)
+        {
+            if (arr.Count == 0)
+            {
+                //canvas.Image = new Bitmap(canvas.Width, canvas.Height);
+                //g = Graphics.FromImage(canvas.Image);
+                g.DrawLine(pen, left, right);
+                canvas.Invalidate();
+                //canvas.Refresh();
+            }
+            else
+            {
+                Point far = far_point(left, right, arr);
+                List<Point> new_arr = arr;
+                new_arr.Remove(far);
+                List<Point> below_left = below_points(left, far, new_arr);
+                List<Point> below_right = below_points(far, right, new_arr);
+                down_border(left, far, below_left);
+                down_border(far, right, below_right);
+            }
+        }
+
+        private int what_side(Point from, Point to, Point where)
+        {
+            Point new_line = new Point(to.X - from.X, to.Y - from.Y);
+            Point new_point = new Point(where.X - from.X, where.Y - from.Y);
+
+
+            if ((new_point.Y * new_line.X - new_point.X * new_line.Y) < 0)
+                return 1;
+            else if ((new_point.Y * new_line.X - new_point.X * new_line.Y) > 0)
+                return -1;
+            else return 0;
+        }
+
+        private Point far_point(Point left, Point right, List<Point> arr)
+        {
+            double distance = 0;
+            Point far = new Point();
+            for (int i = 0; i < arr.Count; ++i)
+            {
+                double temp_dist = Math.Abs((arr[i].X - left.X) * (right.Y - left.Y) - (arr[i].Y - left.Y) * (right.X - left.X));
+                if (temp_dist > distance)
+                {
+                    distance = temp_dist;
+                    far = arr[i];
+                }
+            }
+            return far;
+        }
+    }
+}
